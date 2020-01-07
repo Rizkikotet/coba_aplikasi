bb3de38a973c4ed5ae1d1ad15e71bac98ce85b02
Rizkikotet <47940350+Rizkikotet@users.noreply.github.com>
2020-01-07 14:38:11 +0700

    Add files via upload

---
+++ dist/precache-manifest.c2727ba306870a67c4b3a3c4172c9a61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ba7756ad9c39d47b5a",
+    "url": "/css/chunk-vendors.bfb3a739.css"
+  },
+  {
+    "revision": "ead77ebfb9983e0d8adb1b7b8989b9a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22875b60ac669119a685",
+    "url": "/js/app.c9b06548.js"
+  },
+  {
+    "revision": "25ba7756ad9c39d47b5a",
+    "url": "/js/chunk-vendors.2fec1f89.js"
+  },
+  {
+    "revision": "00c227c72a57cd25fbcb9f15e493c849",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
